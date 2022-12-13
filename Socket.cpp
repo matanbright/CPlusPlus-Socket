bb2303e050b6588507221a33e18eb96e91ec7ca6
Matan Brightbert <36795659+matanbright@users.noreply.github.com>
2022-12-13 15:10:12 +0200

    Add files via upload

---
+++ Socket.cpp
@@ -0,0 +1,137 @@
+#include "Socket.hpp"
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+
+Socket::Socket(AddressFamily addressFamily, Protocol protocol) {
+    this->addressFamily = addressFamily;
+    int fileDescriptor = socket(
+        (int) addressFamily,
+        (protocol == Socket::Protocol::TCP ? SOCK_STREAM : (protocol == Socket::Protocol::UDP ? SOCK_DGRAM : 0)),
+        0
+    );
+    if (fileDescriptor < 0)
+        throw SocketInitializationFailedException();
+    this->fileDescriptor = fileDescriptor;
+}
+
+Socket::Socket(int fileDescriptor) {
+    this->fileDescriptor = fileDescriptor;
+}
+
+Socket::~Socket() {
+    this->close();
+}
+
+void Socket::bind(const char* ipAddressString, int portNumber) {
+    sockaddr_storage ipEndpoint = Socket::getIpEndpoint(this->addressFamily, ipAddressString, portNumber);
+    if (::bind(this->fileDescriptor, (sockaddr*) &ipEndpoint, sizeof(ipEndpoint)) < 0)
+        throw UnableToBeBoundToEndpointException();
+}
+
+void Socket::listen(int backlog) {
+    if (::listen(this->fileDescriptor, backlog) < 0)
+        throw UnableToListenForConnectionsException();
+}
+
+Socket Socket::accept() {
+    int fileDescriptor = ::accept(this->fileDescriptor, nullptr, 0);
+    if (fileDescriptor < 0)
+        throw UnableToAcceptConnectionException();
+    return Socket(fileDescriptor);
+}
+
+Socket Socket::accept(char* peerIpAddressString, int* peerPortNumber) {
+    sockaddr_storage peerIpEndpoint;
+    socklen_t peerIpEndpointSize = sizeof(peerIpEndpoint);
+    int fileDescriptor = ::accept(this->fileDescriptor, (sockaddr*) &peerIpEndpoint, &peerIpEndpointSize);
+    if (fileDescriptor < 0)
+        throw UnableToAcceptConnectionException();
+    Socket::parseIpEndpoint(peerIpEndpoint, peerIpAddressString, peerPortNumber);
+    return Socket(fileDescriptor);
+}
+
+void Socket::connect(const char* ipAddressString, int portNumber) {
+    sockaddr_storage ipEndpoint = Socket::getIpEndpoint(this->addressFamily, ipAddressString, portNumber);
+    if (::connect(this->fileDescriptor, (sockaddr*) &ipEndpoint, sizeof(ipEndpoint)) < 0)
+        throw UnableToConnectToEndpointException();
+}
+
+int Socket::receive(char* receivedDataBuffer, size_t receivedDataBufferSize) {
+    int bytesReceived = recv(this->fileDescriptor, receivedDataBuffer, receivedDataBufferSize, 0);
+    if (bytesReceived < 0)
+        throw UnableToReceiveDataException();
+    return bytesReceived;
+}
+
+int Socket::receiveFrom(char* receivedDataBuffer, size_t receivedDataBufferSize, char* sourceIpAddressString, int* sourcePortNumber) {
+    sockaddr_storage sourceIpEndpoint;
+    socklen_t sourceIpEndpointSize = sizeof(sourceIpEndpoint);
+    int bytesReceived = recvfrom(this->fileDescriptor, receivedDataBuffer, receivedDataBufferSize, 0, (sockaddr*) &sourceIpEndpoint, &sourceIpEndpointSize);
+    if (bytesReceived < 0)
+        throw UnableToReceiveDataException();
+    Socket::parseIpEndpoint(sourceIpEndpoint, sourceIpAddressString, sourcePortNumber);
+    return bytesReceived;
+}
+
+void Socket::send(const char* data, size_t dataSize) {
+    if (::send(this->fileDescriptor, data, dataSize, 0) < 0)
+        throw UnableToSendDataException();
+}
+
+void Socket::sendTo(const char* data, size_t dataSize, const char* destinationIpAddressString, int destinationPortNumber) {
+    sockaddr_storage destinationIpEndpoint = Socket::getIpEndpoint(this->addressFamily, destinationIpAddressString, destinationPortNumber);
+    if (::sendto(this->fileDescriptor, data, dataSize, 0, (sockaddr*) &destinationIpEndpoint, sizeof(destinationIpEndpoint)) < 0)
+        throw UnableToSendDataException();
+}
+
+void Socket::shutdown(ShutdownManner shutdownManner) {
+    if (::shutdown(this->fileDescriptor, (int) shutdownManner) < 0)
+        throw UnableToShutdownException();
+}
+
+void Socket::close() {
+    try {
+        this->shutdown(ShutdownManner::SHUTDOWN_ALL);
+    } catch (exception e) {}
+    ::close(this->fileDescriptor);
+}
+
+// This function is used for creating a 'sockaddr_storage' struct from an IP-address string and a port number.
+sockaddr_storage Socket::getIpEndpoint(AddressFamily addressFamily, const char* ipAddressString, int portNumber) {
+    sockaddr_storage ipEndpoint;
+    ipEndpoint.ss_family = (int) addressFamily;
+    in6_addr ipAddress; // This struct is big enough to hold both IPv4 and IPv6 addresses.
+    if (inet_pton((int) addressFamily, ipAddressString, &ipAddress) != 1)
+        throw InvalidIpAddressException();
+    switch (addressFamily) {
+        case INET:
+            ((sockaddr_in*) &ipEndpoint)->sin_addr = *((in_addr*) &ipAddress);
+            ((sockaddr_in*) &ipEndpoint)->sin_port = htons(portNumber);
+            break;
+        case INET6:
+            ((sockaddr_in6*) &ipEndpoint)->sin6_addr = ipAddress;
+            ((sockaddr_in6*) &ipEndpoint)->sin6_port = htons(portNumber);
+            break;
+    }
+    return ipEndpoint;
+}
+
+// This function is used for parsing an IP-address string and a port number from a 'sockaddr_storage' struct.
+void Socket::parseIpEndpoint(sockaddr_storage ipEndpoint, char* ipAddressString, int* portNumber) {
+    AddressFamily addressFamily = (AddressFamily) ipEndpoint.ss_family;
+    in6_addr ipAddress; // This struct is big enough to hold both IPv4 and IPv6 addresses.
+    switch (addressFamily) {
+        case INET:
+            *((in_addr*) &ipAddress) = ((sockaddr_in*) &ipEndpoint)->sin_addr;
+            *portNumber = ntohs(((sockaddr_in*) &ipEndpoint)->sin_port);
+            break;
+        case INET6:
+            *((in6_addr*) &ipAddress) = ((sockaddr_in6*) &ipEndpoint)->sin6_addr;
+            *portNumber = ntohs(((sockaddr_in6*) &ipEndpoint)->sin6_port);
+            break;
+    }
+    if (inet_ntop((int) addressFamily, &ipAddress, ipAddressString, INET6_ADDRSTRLEN) == nullptr)
+        throw InvalidIpEndpointException();
+}
---
+++ Socket.hpp
@@ -0,0 +1,123 @@
+#if !defined(SOCKET_H)
+#define SOCKET_H
+
+
+
+#include <stdexcept>
+#include <sys/socket.h>
+
+using namespace std;
+
+
+class Socket {
+
+    public: class SocketInitializationFailedException : public exception {
+        private: const char* const EXCEPTION_MESSAGE = "Error: Socket initialization has been failed!";
+        public: const char* what() const noexcept override {
+            return EXCEPTION_MESSAGE;
+        }
+    };
+    
+    public: class UnableToBeBoundToEndpointException : public exception {
+        private: const char* const EXCEPTION_MESSAGE = "Error: The socket is unable to be bound to the specified endpoint!";
+        public: const char* what() const noexcept override {
+            return EXCEPTION_MESSAGE;
+        }
+    };
+
+    public: class UnableToListenForConnectionsException : public exception {
+        private: const char* const EXCEPTION_MESSAGE = "Error: The socket is unable to listen for connections!";
+        public: const char* what() const noexcept override {
+            return EXCEPTION_MESSAGE;
+        }
+    };
+
+    public: class UnableToAcceptConnectionException : public exception {
+        private: const char* const EXCEPTION_MESSAGE = "Error: The socket is unable to accept a connection!";
+        public: const char* what() const noexcept override {
+            return EXCEPTION_MESSAGE;
+        }
+    };
+
+    public: class UnableToConnectToEndpointException : public exception {
+        private: const char* const EXCEPTION_MESSAGE = "Error: The socket is unable to connect to the specified endpoint!";
+        public: const char* what() const noexcept override {
+            return EXCEPTION_MESSAGE;
+        }
+    };
+
+    public: class UnableToReceiveDataException : public exception {
+        private: const char* const EXCEPTION_MESSAGE = "Error: The socket is unable to receive data!";
+        public: const char* what() const noexcept override {
+            return EXCEPTION_MESSAGE;
+        }
+    };
+
+    public: class UnableToSendDataException : public exception {
+        private: const char* const EXCEPTION_MESSAGE = "Error: The socket is unable to send data!";
+        public: const char* what() const noexcept override {
+            return EXCEPTION_MESSAGE;
+        }
+    };
+
+    public: class UnableToShutdownException : public exception {
+        private: const char* const EXCEPTION_MESSAGE = "Error: The socket is unable to shutdown!";
+        public: const char* what() const noexcept override {
+            return EXCEPTION_MESSAGE;
+        }
+    };
+
+    public: class InvalidIpAddressException : public exception {
+        private: const char* const EXCEPTION_MESSAGE = "Error: The specified IP address is not valid for the selected address family!";
+        public: const char* what() const noexcept override {
+            return EXCEPTION_MESSAGE;
+        }
+    };
+
+    public: class InvalidIpEndpointException : public exception {
+        private: const char* const EXCEPTION_MESSAGE = "Error: The specified IP endpoint is not valid!";
+        public: const char* what() const noexcept override {
+            return EXCEPTION_MESSAGE;
+        }
+    };
+
+    public: enum AddressFamily {
+        INET = AF_INET,
+        INET6 = AF_INET6
+    };
+
+    public: enum Protocol {
+        TCP,
+        UDP
+    };
+
+    public: enum ShutdownManner {
+        SHUTDOWN_READ = SHUT_RD,
+        SHUTDOWN_WRITE = SHUT_WR,
+        SHUTDOWN_ALL = SHUT_RDWR
+    };
+
+    private: int fileDescriptor;
+    private: AddressFamily addressFamily;
+
+    public: Socket(AddressFamily addressFamily, Protocol protocol);
+    private: Socket(int fileDescriptor);
+    public: ~Socket();
+    public: void bind(const char* ipAddressString, int portNumber);
+    public: void listen(int backlog);
+    public: Socket accept();
+    public: Socket accept(char* peerIpAddressString, int* peerPortNumber);
+    public: void connect(const char* ipAddressString, int portNumber);
+    public: int receive(char* receivedDataBuffer, size_t receivedDataBufferSize);
+    public: int receiveFrom(char* receivedDataBuffer, size_t receivedDataBufferSize, char* sourceIpAddressString, int* sourcePortNumber);
+    public: void send(const char* data, size_t dataSize);
+    public: void sendTo(const char* data, size_t dataSize, const char* destinationIpAddressString, int destinationPortNumber);
+    public: void shutdown(ShutdownManner shutdownManner);
+    public: void close();
+    private: static sockaddr_storage getIpEndpoint(AddressFamily addressFamily, const char* ipAddressString, int portNumber);
+    private: static void parseIpEndpoint(sockaddr_storage ipEndpoint, char* ipAddressString, int* portNumber);
+};
+
+
+
+#endif
